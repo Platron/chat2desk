4d7ad4849e91e8c8f586fb54e543223cd3868bd4
lashnev <a.lashnev@platron.ru>
2017-09-01 19:09:05 +0300

    Исправление использования тестов

--- tests/integration/IntegrationTestBase.php
+++ tests/integration/IntegrationTestBase.php
@@ -2,14 +2,10 @@
 
 namespace Platron\Chat2desk\tests\integration;
 
-use Platron\Chat2desk\clients\BaseClient;
 use Platron\Chat2desk\tests\integration\UserSettings;
-use Platron\Chat2desk\clients\BaseClient;
 
 abstract class IntegrationTestBase extends \PHPUnit_Framework_TestCase {
-    
-    /** @var BaseClient */
-    protected $client;
+
     /** @var string секретная строка из ЛК */
     protected $authString;
     /** @var string Номер телефона, на который будет отправляться сообщения */
--- tests/integration/whatsapp/MessagesPostTest.php
+++ tests/integration/whatsapp/MessagesPostTest.php
@@ -2,16 +2,19 @@
 
 namespace Platron\Chat2desk\tests\integration\whatsapp;
 
+use Platron\Chat2desk\clients\PostClient;
 use Platron\Chat2desk\services\MessagesPostServiceRequest;
 use Platron\Chat2desk\services\MessagesPostServiceResponse;
 
 class MessagesPostTest extends WhatsappTestBase {
     public function testSendRequest(){
+        $client = new PostClient($this->authString);
+        
         $service = new MessagesPostServiceRequest();
         $service->setClientId($clientId);
         $service->setText('Test');
         $service->setTransport($this->getTransport());
         
-        $this->assertTrue((new MessagesPostServiceResponse($this->client->sendRequest($service)))->isValid());
+        $this->assertTrue((new MessagesPostServiceResponse($client->sendRequest($service)))->isValid());
     }
 }
