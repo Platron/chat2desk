7dd7a3af769623282fef002194494d21de9c2809
alexey lashnev <a.lashnev@platron.ru>
2017-09-02 12:46:03 +0300

    Удаление файла с настроками для тестов

--- tests/integration/UserSettings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Platron\Chat2desk\tests\integration;
-
-class UserSettings {
-    const 
-        API_STRING = '3835934002dd5f67308f7dfd1a4d20',
-        PHONE_TO_SEND_MESSAGES = '79267449959';
-}
