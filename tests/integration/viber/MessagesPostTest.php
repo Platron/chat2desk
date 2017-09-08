d044945562da7aaa3b45cf2d8d0847fdbfac55d5
lashnev <a.lashnev@platron.ru>
2017-09-08 14:17:41 +0300

    Повторное добавление клиента вызывает повторную задержку. При этом выдается id клиента, который уже был добавлен и ошибок не возникает

--- tests/integration/viber/MessagesPostTest.php
+++ tests/integration/viber/MessagesPostTest.php
@@ -6,15 +6,27 @@ use Platron\Chat2desk\services\messages\MessagesPostServiceRequest;
 use Platron\Chat2desk\services\messages\MessagesPostServiceResponse;
 use Platron\Chat2desk\services\clients\ClientsPostServiceResponse;
 use Platron\Chat2desk\services\clients\ClientsPostServiceRequest;
+use Platron\Chat2desk\services\clients\ClientsGetServiceRequest;
+use Platron\Chat2desk\services\clients\ClientsGetServiceResponse;
 
 class MessagesPostTest extends ViberBaseTest {
     public function testSendRequest(){
+        
+        $service = new ClientsGetServiceRequest();
+		$service->setPhone($this->phoneTo);
+		$getResponseClient = new ClientsGetServiceResponse($service->sendRequest($this->authString));
+        
+        if(empty($getResponseClient->clients[0])){
 		$servicePostClient = new ClientsPostServiceRequest();
         $servicePostClient->setPhone($this->phoneTo);
         $servicePostClient->setTransport($this->getTransport());
 		
 		$createdClient = new ClientsPostServiceResponse($servicePostClient->sendRequest($this->authString));
-		
+        }
+        else {
+            $createdClient = $getResponseClient->clients[0];
+        }
+        
         $servicePostMessage = new MessagesPostServiceRequest();
         $servicePostMessage->setClientId($createdClient->id);
         $servicePostMessage->setText('Test');
--- tests/integration/whatsapp/MessagesPostTest.php
+++ tests/integration/whatsapp/MessagesPostTest.php
@@ -6,15 +6,27 @@ use Platron\Chat2desk\services\messages\MessagesPostServiceRequest;
 use Platron\Chat2desk\services\messages\MessagesPostServiceResponse;
 use Platron\Chat2desk\services\clients\ClientsPostServiceResponse;
 use Platron\Chat2desk\services\clients\ClientsPostServiceRequest;
+use Platron\Chat2desk\services\clients\ClientsGetServiceRequest;
+use Platron\Chat2desk\services\clients\ClientsGetServiceResponse;
 
 class MessagesPostTest extends WhatsappBaseTest {
     public function testSendRequest(){
-		$servicePostClient = new ClientsPostServiceRequest();
-        $servicePostClient->setPhone($this->phoneTo);
-        $servicePostClient->setTransport($this->getTransport());
-		
-		$createdClient = new ClientsPostServiceResponse($servicePostClient->sendRequest($this->authString));
-		
+        $service = new ClientsGetServiceRequest();
+		$service->setPhone($this->phoneTo);
+
+		$getClientResponse = new ClientsGetServiceResponse($service->sendRequest($this->authString));
+        
+        if(empty($getClientResponse->clients[0])){
+            $servicePostClient = new ClientsPostServiceRequest();
+            $servicePostClient->setPhone($this->phoneTo);
+            $servicePostClient->setTransport($this->getTransport());
+
+            $createdClient = new ClientsPostServiceResponse($servicePostClient->sendRequest($this->authString));
+        }
+        else {
+            $createdClient = $getClientResponse->clients[0];
+        }
+        
         $servicePostMessage = new MessagesPostServiceRequest();
         $servicePostMessage->setClientId($createdClient->id);
         $servicePostMessage->setText('Test');
